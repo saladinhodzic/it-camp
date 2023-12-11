e67ee296014d81219893a9182cc90d88729b646e
saladinhodzic <hsaladin06@gmail.com>
2023-12-11 23:44:15 +0100

    ClassList

--- javascript/Vezba/JS,CSS,HTML/vezba.js
+++ javascript/Vezba/JS,CSS,HTML/vezba.js
@@ -2,3 +2,29 @@ const dugme = document
   .querySelector(".js-button")
   .classList.contains("js-button");
 console.log(dugme);
+// classList.contains() nam provera imamo li takvu klasu u html elementu
+// document.querySelector(".is-toggled").classList.remove("is-toggled");
+turnEverythingOff();
+function turnButtonOn(selector) {
+  const button = document.querySelector(selector);
+  if (!button.classList.contains("is-toggled")) {
+    turnOffButton();
+
+    button.classList.add("is-toggled");
+  } else if (button.classList.contains("is-toggled")) {
+    button.classList.remove("is-toggled");
+  }
+}
+
+function turnOffButton() {
+  const isTherePrevious = document.querySelector(".is-toggled");
+  if (isTherePrevious) {
+    isTherePrevious.classList.remove("is-toggled");
+  }
+}
+
+function turnEverythingOff() {
+  document.querySelector(".gaming").classList.remove("is-toggled");
+  document.querySelector(".music").classList.remove("is-toggled");
+  document.querySelector(".tech").classList.remove("is-toggled");
+}

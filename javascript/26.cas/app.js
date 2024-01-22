7061b966e34089bcabd54528031d6ba2fa0e1504
saladinhodzic <hsaladin06@gmail.com>
2024-01-22 21:17:59 +0100

    Numbers

---
+++ javascript/26.cas/app.js
@@ -0,0 +1,40 @@
+// NUMBERS
+
+const x  = 0.1
+const y = 0.2
+console.log(x+y)
+
+// Racunanje realnih brojeva nije uvek tacno u Javascriptu
+
+console.log(5+5)
+console.log(5+"5")
+console.log(5/"5")
+console.log(5-"5a")
+
+console.log(5/0)//infinityt
+
+// Hexadecimalni brojevi u js-u se interpretiraju sa prefiksom 0x
+
+// const white = 0x#fff
+
+// Brojevi takodje mogu biti definisani preko new keyword i oni su tipa objekt
+
+let z = new Number(0.3)
+let w = 0.3
+console.log(z)
+console.log(z===w)
+
+function nesto(string){
+    const index_= email.indexOf('_')
+    const indexDot= email.indexOf('.')
+    const hashtag= email.indexOf('@')
+    const cutStart = index_ === -1 ? indexDot : index_
+    let cut = email.slice(cutStart,hashtag)
+
+
+    
+return string.replace()
+}
+console.log(nesto("saladin_hodzic@gmail.com"))
+
+// w3.resors

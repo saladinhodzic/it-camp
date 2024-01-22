2370b4e0d58e5f930c82f03055cae178d85bb18d
saladinhodzic <hsaladin06@gmail.com>
2024-01-22 20:27:42 +0100

    domaci-ispravak

--- javascript/Domaci zadatak/Stringovi/string5.js
+++ javascript/Domaci zadatak/Stringovi/string5.js
@@ -8,17 +8,39 @@
 
 function ispitivanje(string){
 const recenica = 'ko osvaja'
+recenicaGlobal = /ko osvaja/g
 
+const pojavljivanja = string.match(recenicaGlobal)
 
-
-for(i  = 0;i<string.length;i++){
     if(!string.includes(recenica)){
         return "Nismo pronasli dati string"
-    }else if (string.includes(recenica)){
-        return string.match(/ko osvaja/g).length
-    }else if(string.includes(recenica)){}
-}
+    }else if(pojavljivanja.length > 1){
+        return string.lastIndexOf(recenica)
+    }else {
+        return string.indexOf(recenica)
+
+    }
 }
 console.log(ispitivanje("ligu sampiona"))
 console.log(ispitivanje("ligu ko osvaja"))
 console.log(ispitivanje("ko osvaja ligu ko osvaja"))
+
+function index(recenica){
+    const dveReci = 'ko osvaja'
+
+    const brojPonavljanja = recenica.includes(dveReci)
+    ?recenica.match(/ko osvaja/g).length
+    :0
+    if(!recenica.includes(dveReci)){
+        return "Nismo pronasli dati string"
+    }else if (brojPonavljanja >1){
+        return `Prvo pojavljivanje ${recenica.indexOf(dveReci)}
+        Zadnje pojavljivanje ${recenica.lastIndexOf(dveReci)}`
+    }else{
+        return recenica.indexOf(dveReci)
+    }
+}
+
+console.log(index("ligu sampiona"))
+console.log(index("ligu ko osvaja"))
+console.log(index("ko osvaja ligu ko osvaja"))

c82050bf74d2e2fdc09dc2d75cc79aa22424375a
saladinhodzic <hsaladin06@gmail.com>
2024-01-27 16:25:37 +0100

    Vezbanje

---
+++ javascript/Domaci zadatak/Numbers/vezbe.js
@@ -0,0 +1,14 @@
+// Write a JavaScript function to round a number to decimal place.
+
+function roundNumber(broj){
+    return Math.round(broj)
+}
+console.log(roundNumber(2.356))
+
+// Pitagorina teorema
+function pitagorinaTeorema(a,b){
+    let c = a*a + b*b
+    return Math.sqrt(c)
+}
+
+console.log(pitagorinaTeorema(3,4))

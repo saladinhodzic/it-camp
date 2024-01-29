296d870c7659d6c084d5b378a71f91eedb53e743
saladinhodzic <hsaladin06@gmail.com>
2024-01-29 21:15:57 +0100

    29.cas - obnavljanje

---
+++ javascript/29.cas/app.js
@@ -0,0 +1,1 @@
+// Obnavljanje znanja
--- javascript/Domaci zadatak/Numbers/vezbe.js
+++ javascript/Domaci zadatak/Numbers/vezbe.js
@@ -1,14 +1,23 @@
 // Write a JavaScript function to round a number to decimal place.
 
-function roundNumber(broj){
-    return Math.round(broj)
+function roundNumber(broj) {
+  return Math.round(broj);
 }
-console.log(roundNumber(2.356))
+console.log(roundNumber(2.356));
 
 // Pitagorina teorema
-function pitagorinaTeorema(a,b){
-    let c = a*a + b*b
-    return Math.sqrt(c)
+function pitagorinaTeorema(a, b) {
+  let c = a * a + b * b;
+  return Math.sqrt(c);
 }
 
-console.log(pitagorinaTeorema(3,4))
+console.log(pitagorinaTeorema(3, 4));
+
+// Write a JavaScript function to generate a random integer.
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+console.log(randomNumber(3, 10));
+console.log(randomNumber(50, 100));

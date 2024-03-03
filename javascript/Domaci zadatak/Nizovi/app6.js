fc12ef866d61f33bb1b0e066e49ecd560f48c362
saladinhodzic <hsaladin06@gmail.com>
2024-03-03 16:22:36 +0100

    LeetCode-zadatak 2.

--- javascript/Domaci zadatak/Nizovi/app6.js
+++ javascript/Domaci zadatak/Nizovi/app6.js
@@ -9,3 +9,27 @@ function findIndex(niz) {
   return noviNiz;
 }
 console.log(findIndex(["apple", "banana", "grape", "grape"]));
+
+// dva niza spojiti i dati njihov zbir u obrnutom redosledu
+
+const prviNiz = [1, 2, 3];
+const drugiNiz = [5, 8, 3];
+let rezultat = [];
+let carry = [];
+for (i = prviNiz.length - 1; i >= 0; i--) {
+  let nesto = prviNiz[i] + drugiNiz[i];
+
+  if (carry[i + 1] == 1) {
+    rezultat.push(nesto + 1);
+    carry.push(0);
+  } else if (nesto >= 10) {
+    carry.push(1);
+    rezultat.push(0);
+  } else {
+    carry.push(0);
+    rezultat.push(nesto);
+  }
+  console.log(carry);
+}
+
+console.log(rezultat.reverse());

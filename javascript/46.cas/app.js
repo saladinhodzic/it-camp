7abbc732b258359b05d468bcf7a58d9e9266ac31
saladinhodzic <hsaladin06@gmail.com>
2024-03-16 20:48:32 +0100

    Priprema za test

---
+++ javascript/46.cas/app.js
@@ -0,0 +1,35 @@
+// Priprema za test
+
+// Test iz dva dela
+
+// Prvi deo - 10 teoriskih pitanja iz oblasti, najvise pitanja iz simple JS-a i dva tri pitanja iz advanced-medium
+
+// 20min
+
+// Select a primitve JS type
+// What are complex types in JS? abcd
+// What are the scopes of const,let and var
+// What are the difference between const and let..
+// What is the result of the code console.log("yes"||"no")
+// What is the result of the code if(true){console.log(true)}
+// What are the loops used in JS?
+// What is an object in JS?
+// How do dynamically access values inside of an object?
+// What are the property accessor for object?
+// Which are the array functions-methods?
+// What does this code do?[].map(()=>{})
+// Advanced only defintions of concepts.
+
+// Drugi deo - prakticni deo dva pitanja
+
+// 40min
+
+// Prvo pitanje
+
+// Bice vise algoritamskog tipa gde imate neku obradu podataka a vi treba nesto sa tim podacima da uradite
+
+// Drugo pitanje
+
+// Treba fetchati nesto sa nekog apija i onda to prikazati na stranici(moguca dodata komplikacija gde cete na neki event fetchati podatke)
+
+// Q&A

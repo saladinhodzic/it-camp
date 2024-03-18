3d98127a31dd88d60ec47516576c50c0761ec2f0
saladinhodzic <hsaladin06@gmail.com>
2024-03-19 00:08:23 +0100

    Projekat-Vezbanje

--- javascript/Vezba/tic tac toe/app.js
+++ javascript/Vezba/tic tac toe/app.js
@@ -1,9 +1,55 @@
 const cells = document.querySelectorAll(".cell");
-const texts = document.querySelectorAll(".cell p");
+const resultScreen = "result";
+const resultScreenJS = document.querySelector(".resultJs");
+const classX = "x";
+const classCircle = "circle";
+const possibleWins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+let circleTurn;
 cells.forEach((cell) => {
   cell.addEventListener("click", handleClick, { once: true });
 });
 
-function handleClick() {
-  texts.forEach((text) => {});
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? classCircle : classX;
+  markCell(cell, currentClass);
+
+  if (checkWin(currentClass)) {
+    showScreen();
+  } else {
+    swapTurn();
+  }
+
+  //   checkWin(currentClass);
+}
+
+function markCell(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurn() {
+  circleTurn = !circleTurn;
+}
+
+function checkWin(currentClass) {
+  return possibleWins.some((combination) => {
+    return combination.every((indeks) => {
+      return cells[indeks].classList.contains(currentClass);
+    });
+  });
+}
+
+function showScreen() {
+  resultScreenJS.classList.add(resultScreen);
+  const wrapper = document.querySelector(".wrapper");
+  wrapper.style = "display:none;";
 }

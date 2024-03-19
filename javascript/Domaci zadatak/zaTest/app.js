fd5a424b06d8d9c755ff76bcd19695c599fa2de0
saladinhodzic <hsaladin06@gmail.com>
2024-03-19 19:10:14 +0100

    Vezbanje

---
+++ javascript/Domaci zadatak/zaTest/app.js
@@ -0,0 +1,12 @@
+// Objects
+
+const object = {
+  nesto: "yes",
+  neko: "biloSta",
+};
+
+const neko = "neko";
+
+console.log(object.nesto);
+console.log(object[neko]);
+console.log(object["nesto"]);

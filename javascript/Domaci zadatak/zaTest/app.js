d5226a52fd470695ea9636053c970aff7c5f69fe
saladinhodzic <hsaladin06@gmail.com>
2024-03-20 20:08:32 +0100

    Vezbanje objekti

--- javascript/Domaci zadatak/zaTest/app.js
+++ javascript/Domaci zadatak/zaTest/app.js
@@ -10,3 +10,58 @@ const neko = "neko";
 console.log(object.nesto);
 console.log(object[neko]);
 console.log(object["nesto"]);
+console.log(object["neko"]);
+
+// niz objekata
+
+const mockData = [
+  {
+    name: "John",
+    surname: "Doe",
+    age: 25,
+    address: "123 Main St, Cityville",
+  },
+  {
+    name: "Alice",
+    surname: "Smith",
+    age: 30,
+    address: "456 Oak St, Townsville",
+  },
+  {
+    name: "Bob",
+    surname: "Johnson",
+    age: 22,
+    address: "789 Pine St, Villagetown",
+  },
+  {
+    name: "Eva",
+    surname: "Brown",
+    age: 28,
+    address: "101 Elm St, Hamletsville",
+  },
+];
+
+// check if any of the people can access only after 25
+
+// const check = mockData.some((value) => {
+//   if (value["age"] > 25) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+const check = mockData.some((value) => {
+  return value["age"] > 25;
+});
+
+console.log(check);
+
+// return object person from Townsville
+
+const checkTownsville = mockData.filter((value) => {
+  if (value["address"].includes("Townsville")) {
+    return true;
+  }
+});
+
+console.log(checkTownsville);

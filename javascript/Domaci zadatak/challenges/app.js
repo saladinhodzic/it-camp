526f706e3bd7dcbe244e2100564da3323f5615d4
saladinhodzic <hsaladin06@gmail.com>
2024-04-11 19:30:40 +0200

    JSChallenger

--- javascript/Domaci zadatak/challenges/app.js
+++ javascript/Domaci zadatak/challenges/app.js
@@ -33,3 +33,36 @@ function myFunction(a) {
 }
 
 console.log(myFunction(niz3));
+
+// Write a function that takes an array of numbers as argument. It should return the average of the numbers.
+const niz4 = [1, 2, 3, 6, 7, 8];
+function findAverage(niz) {
+  return (
+    niz.reduce((acc, value) => {
+      return acc + value;
+    }, 0) / niz.length
+  );
+}
+// function findAverage(niz) {
+//   const zbir = niz.reduce((acc, value) => {
+//     return acc + value;
+//   }, 0);
+//   return zbir / niz.length;
+// }
+console.log(findAverage(niz4));
+
+// Write a function that takes an array of strings as argument. Return the longest string.
+const strings = ["help", "goofy", "excuse", "me"];
+
+function longestWord(arr) {
+  let long = "";
+  arr.forEach((value) => {
+    if (!long) {
+      long = value;
+    } else if (long.length < value.length) {
+      long = value;
+    }
+  });
+  return long;
+}
+console.log(longestWord(strings));

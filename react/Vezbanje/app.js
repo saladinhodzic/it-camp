8441ae04ae4c12c4d6690dfa979ae74ab19571b5
saladinhodzic <hsaladin06@gmail.com>
2024-05-01 16:43:45 +0200

    Buliding a static page with React,learning components

--- react/Vezbanje/app.js
+++ react/Vezbanje/app.js
@@ -1,12 +1,18 @@
-const page = (
-  <div>
-    <h1>This is written in JSX</h1>
-    <ul>
-      <li>Contact</li>
-      <li>About</li>
-      <li>Menu</li>
-    </ul>
-  </div>
-);
-const root = document.getElementById("root");
-ReactDOM.render(page, root);
+function PageRender() {
+  return (
+    <div>
+      <img width="50px" src="./React-icon.svg" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Realised in 2013</li>
+        <li>Created by Jordan Walke</li>
+        <li>Has over 100k stars on GitHub</li>
+        <li>Maintained by Facebook</li>
+        <li>Powers thousands of apps</li>
+      </ul>
+    </div>
+  );
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<PageRender />);
+
+console.log(page);

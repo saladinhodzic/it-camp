0c3f5c0f2856d188da3aa62f18c3ae18001d4ed1
saladinhodzic <hsaladin06@gmail.com>
2024-05-06 18:59:30 +0200

    Breaking page into components

--- react/Vezbanje/app.js
+++ react/Vezbanje/app.js
@@ -1,18 +1,35 @@
-function MyCustomPage() {
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./React-icon.svg" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return <small>@2024 Saki development. All rights reserved.</small>;
+}
+
+function MainComponent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./React-icon.svg" width="40px" />
-        </nav>
-      </header>
       <h1>Reasons i learn React</h1>
       <ol>
         <li>idk</li>
         <li>Popular</li>
         <li>Profitable</li>
       </ol>
-      <small>@2024 Saki development. All rights reserved.</small>
+    </div>
+  );
+}
+function MyCustomPage() {
+  return (
+    <div>
+      <Header />
+      <MainComponent />
+      <Footer />
     </div>
   );
 }

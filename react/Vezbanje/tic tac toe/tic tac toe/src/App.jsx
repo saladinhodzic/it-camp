52298655a888816ad24d01612f03456188324a99
saladinhodzic <hsaladin06@gmail.com>
2024-05-09 08:33:17 +0200

    Adding more things

--- react/Vezbanje/tic tac toe/tic tac toe/src/App.jsx
+++ react/Vezbanje/tic tac toe/tic tac toe/src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 import "./App.css";
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("X");
-  }
+function Square({ value, onSquareClicked }) {
   return (
-    <button onClick={handleClick} className="square">
+    <button className="square" onClick={onSquareClicked}>
       {value}
     </button>
   );
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const newSquares = squares.slice();
+    newSquares[0] = "X";
+    setSquares(newSquares);
+  }
   return (
     <div className="board">
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClicked={handleClick} />
+        <Square value={squares[1]} onSquareClicked={handleClick} />
+        <Square value={squares[2]} onSquareClicked={handleClick} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClicked={handleClick} />
+        <Square value={squares[4]} onSquareClicked={handleClick} />
+        <Square value={squares[5]} onSquareClicked={handleClick} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClicked={handleClick} />
+        <Square value={squares[7]} onSquareClicked={handleClick} />
+        <Square value={squares[8]} onSquareClicked={handleClick} />
       </div>
     </div>
   );

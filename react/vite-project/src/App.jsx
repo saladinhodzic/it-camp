60bfc0f9afdd1df7a3f165a3385c0626156f6f5e
saladinhodzic <hsaladin06@gmail.com>
2024-05-18 10:33:58 +0200

    Vezbanje-Profili

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-const slike = ["./slike/joseph-gonzalez-iFgRcqHznqg-unsplash.jpg"];
+const slike = [
+  "./src/assets/slike/lik.jpg",
+  "./src/assets/slike/lik2.jpg",
+  "./src/assets/slike/lik3.jpg",
+];
 
 function Profile() {
-  return <img src={slike[0]} />;
+  return (
+    <div className="profiles">
+      <div className="card">
+        <img className="profile-1 profile" src={slike[0]} />
+        <h1>Carlos Hernandez</h1>
+        <p>Carlos is young man but he transfers drugs</p>
+      </div>
+      <div className="card">
+        <img className="profile-2 profile" src={slike[1]} />
+        <h1>Naruto Saske</h1>
+        <p>Fast but doesnt think much</p>
+      </div>
+      <div className="card">
+        <img className="profile-3 profile" src={slike[2]} />
+        <h1>Sakura Nesto</h1>
+        <p>Thinks fast but slow</p>
+      </div>
+    </div>
+  );
 }
 
 export default function App() {
